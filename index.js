888ed526989224545dfc004fc1ac9fe869cbf362
Brian <bstanton773@gmail.com>
2022-06-27 14:07:50 -0500

    Add ejs templating and users view

--- index.js
+++ index.js
@@ -6,11 +6,33 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {firstName: 'Brian'})
 })
 
 app.get('/apple', (req, res) => {
     res.send('This is apple')
+})
+
+let users = [
+    {
+        id: 1,
+        username: 'brians',
+        age: 22
+    },
+    {
+        id: 2,
+        username: 'ta7',
+        age: 33
+    },
+    {
+        id: 3,
+        username: 'jumpman23',
+        age: 55
+    }
+]
+
+app.get('/users', (req, res) => {
+    res.render('users', { users })
 })
 
 app.listen(port, () => {
